f79daade5f5881f92bacc57877a2027ad988a2fe
Pavan Amrut Badgujar <pavanabad01@gmail.com>
2024-04-26 21:40:15 +0530

    Numpy Multinomial Distribution

---
+++ numpylib/24_numpymultinomial.py
@@ -0,0 +1,9 @@
+# Multinomial dist : it is a generalization of binomial dist.
+# parameter - n(number of outcome or possibility), pvals(list of possibilities or outcomes), size(shape or outcomesreturned array)
+# Draw out sample fro dice roll
+
+from  numpy import random
+x=random.multinomial(n=6, pvals=[1/6,1/6,1/6,1/6,1/6,1/6])
+print(x)
+
+# multinomial samples will not produce a single value, they will produce one value for each pvals.
