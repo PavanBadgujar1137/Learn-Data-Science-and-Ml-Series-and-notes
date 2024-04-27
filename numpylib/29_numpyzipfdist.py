09f5e2574cfa692775f6e87425db1362fe6991e8
Pavan Amrut Badgujar <pavanabad01@gmail.com>
2024-04-27 11:24:20 +0530

    Numpy Zipf Distribution

---
+++ numpylib/29_numpyzipfdist.py
@@ -0,0 +1,14 @@
+# Zipf Dist: its defination like a common word in english has occurs nearly 1/5 time as of the most hindi words
+# param - a(dist param), size
+# sample for zipf dist with a as z with size 2*3
+from numpy import random
+x=random.zipf(a=2,size=(2,3))
+print(x)
+
+# visualization of zip dist
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+x=random.zipf(a=2,size=1000)
+sns.distplot(x[x<10],kde=False)
+plt.show()
